6f159a86175be946c48ef0048d7369d2ebf2df8a
matdombrock <dombrock.mathieu@gmail.com>
2025-11-10 13:43:48 -0800

    cleanup finder todo

--- fishfinder/finder.fish
+++ fishfinder/finder.fish
@@ -5,7 +5,7 @@
 
 # TODO:
 # - If the file is executable, offer to run it vs edit it.
-# - Its not possible to leave the shell in the final dir unluess this is sourced
+# - Its not possible to leave the shell in the final dir
 #   You can do something like `cd (fishfinder)` from the shell but that breaks editing files
 #   You can also source this file which allows both cd and editing
 
