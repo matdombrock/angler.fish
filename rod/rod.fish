a0494e0317de1e6ad7229a0eb07e98479fa8ab3e
matdombrock <dombrock.mathieu@gmail.com>
2025-11-12 16:40:55 -0800

    rod

--- rod/rod.fish
+++ rod/rod.fish
@@ -1,6 +1,14 @@
 set PROMPT full
+set rod_list
+
+# NOTE:
+# Its probablly slow to define functions inside fish_prompt every time its called.
+# The only reasonable alternative is to export the functions so they are available to fish_prompt
 function fish_prompt
-    if test "$PROMPT" = full
+    set rod_list # Clear rod_list
+
+    set -a rod_list full
+    function full
         set -l userIcon ''
         set -l hostIcon ''
         set -l folderIcon ''
@@ -24,13 +32,18 @@ function fish_prompt
         set_color bryellow
         echo -n "$prompIcon "
         set_color normal
-    else if test "$PROMPT" = minimal
+    end
+
+    set -a rod_list minimal
+    function minimal
         set_color bryellow
         set -l prompIcon λ
         echo -n "$prompIcon "
         set_color normal
-    else
-        # Default prompt
+    end
+
+    set -a rod_list default
+    function default
         set -l last_status $status
         set -l cyan (set_color cyan)
         set -l normal (set_color normal)
@@ -61,14 +74,36 @@ function fish_prompt
 
         echo -n -s ' ' $arrow_color $arrow $normal ' '
     end
+
+    # Check if PROMPT is in rod_list
+    if not contains $PROMPT $rod_list
+        set_color red
+        echo "Warning: PROMPT '$PROMPT' not found in rod_list. Reverting to 'default'."
+        rod # Call with no args to get the list
+        set_color normal
+        # We cant continue without  having some kind of prompt here
+        set PROMPT default
+    end
+
+    eval $PROMPT
+    return
 end
 
-function set_prompt
+function rod
+    if test "$argv[1]" = list; or test "$argv[1]" = l; or test -z "$argv[1]"
+        set_color brmagenta
+        echo "Available prompt styles:"
+        set_color brgreen
+        for prompt_style in $rod_list
+            echo " - $prompt_style"
+        end
+        return
+    end
     set PROMPT $argv[1]
 end
 
 # Appease the LSP
 if test 1 = 0
     fish_prompt
-    set_prompt
+    rod
 end
